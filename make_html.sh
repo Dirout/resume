cbaa37de1b5232926eb11911c8f83ffc0ce89801
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2024-02-11 21:07:03 -0500

    Feb 2024 update

--- make_html.sh
+++ make_html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pdf2htmlEX --external-hint-tool=ttfautohint --bg-format svg -f 1 -l 1 --zoom 1.5 --process-outline 0 'Emil Sayahi.pdf'
-mv -f 'Emil Sayahi.html' index.html
+pdf2htmlEX --external-hint-tool=ttfautohint --bg-format svg -f 1 -l 1 --zoom 1.5 --process-outline 0 'resume.pdf'
+mv -f 'resume.html' index.html
